697f6b4a725e31a1b91982dfd519765e9a83c6a3
rileywoo <mylifeisriley@gmail.com>
2016-08-25 01:13:36 -0700

    Can instantiate classes

--- package/Class.py
+++ package/Class.py
@@ -1,12 +1,16 @@
-class Class:
+class AClass:
     """A class for classes"""
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, subject, number):
+        self.subject = subject
+        self.number = number
         self.lecture_start_time = ""
         self.lecture_end_time = ""
         self.discussions = []
         self.labs = []
+
+    def __str__(self):
+        return self.subject + str(self.number)
 
     def add_discussion(self, dis):
         self.discussions.append(self, dis)
