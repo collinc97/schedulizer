773cc9a208774f056ed199c2d11471fcbc7c3a32
rileywoo <mylifeisriley@gmail.com>
2016-08-30 10:53:51 -0700

    Can now keep searching even if the page says theres no classes for certain criteria

--- package/CollectClassData.py
+++ package/CollectClassData.py
@@ -1,4 +1,7 @@
 from selenium import webdriver, common
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from package import Class
 import re
 
@@ -16,32 +19,45 @@ pickled_data = {}
 # Be sure to specify path to chromedriver in webdriver.Chrone('/here/')
 driver = webdriver.Chrome('/Users/bbc/chromedriver')
 
-for i in range(1, 2): # Eventually change to range(1, len(subject_list))
+for i in range(5, 6): # Eventually change to range(1, len(subject_list))
     driver.get(
         "https://bcsweb.is.berkeley.edu/psc/bcsprd_pub/EMPLOYEE/HRMS/c/COMMUNITY_ACCESS.CLASS_SEARCH.GBL?ucFrom=berkeley")
     driver.find_element_by_name('SSR_CLSRCH_WRK_SUBJECT_SRCH$0').send_keys(subject_list[i])
     driver.find_element_by_name('SSR_CLSRCH_WRK_ACAD_CAREER$2').send_keys("undergraduate")
     driver.find_element_by_name('CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH').click()
-    driver.implicitly_wait(100)
 
-    index = 0
-    classes = []
-    while True:
-        try:
-            class_name = driver.find_element_by_id('win0divSSR_CLSRSLT_WRK_GROUPBOX2GP$' + str(index)).text
-            class_number = class_name.split()[1]
-            test_class = Class.AClass(subject_list[i], class_number)
-            print(test_class)
-            classes.append(test_class)
-            index += 1
-        except common.exceptions.NoSuchElementException:
-            # This took about 45 seconds for selenium to hit, after finding all 4 aerospace classes.
-            # Might need to find a better way/hopefully there is a better way
-            break
+    try:
+        noClassElement = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, "//*[@id='DERIVED_CLSMSG_ERROR_TEXT']")))
+    except:
+        noClassElement = None
 
-    # Put data in the pickled dictionary
-    #for c in classes:
-    #    c.collect_data(driver)
-    #    pickled_data[str(c)] = c
-# closes browser
-driver.close()
+    try:
+        moreThanFiftyClassesElement = WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, "// *[ @ id = 'DERIVED_SSE_DSP_SSR_MSG_TEXT']")))
+    except:
+        moreThanFiftyClassesElement = None
+
+    if moreThanFiftyClassesElement != None:
+        driver.find_element_by_xpath("//*[@id='#ICSave']").click()
+    print "noClassElement is " + str(noClassElement)
+    if noClassElement == None:
+        index = 0
+        classes = []
+        while True:
+            try:
+                class_name = driver.find_element_by_id('win0divSSR_CLSRSLT_WRK_GROUPBOX2GP$' + str(index)).text
+                class_number = class_name.split()[1]
+                test_class = Class.AClass(subject_list[i], class_number)
+                print(test_class)
+                classes.append(test_class)
+                index += 1
+            except common.exceptions.NoSuchElementException:
+                # Okay now this is fast, but for Anthropology it's jumping to this except clause.
+                # It can't find the element "'win0divSSR_CLSRSLT_WRK_GROUPBOX2GP$0'" for some reason.
+                break
+
+                # Put data in the pickled dictionary
+                # for c in classes:
+                #    c.collect_data(driver)
+                #    pickled_data[str(c)] = c
+# Close browser
+#driver.close()
